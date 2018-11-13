13b14eb0abe08f7b92b265f1ae0b2d7f2f4a3362
Garrett Lovell <seanhelmes@Garretts-MacBook-Pro-2.local>
2018-11-13 17:48:05 -0500

    Added Apollo and first query

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
+
+// Connecting to GrapQL API
+const client = new ApolloClient({
+  uri: 'https://api-useast.graphcms.com/v1/cjoft95z37yw401dg1egocbfu/master'
+});
+
+const POSTS_QUERY = gql`
+  query {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`;
+
+// client
+//   .query({
+//     query: testQuery
+//   })
+//   .then(res => console.log(res));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title" />
+            Welcome to React
+          </header>
+          <Query query={POSTS_QUERY}>
+            {({ loading, data }) => {
+              if (loading) return 'Loading...';
+              const { posts } = data;
+              return posts.map(post => <h1>{post.title}</h1>);
+            }}
+          </Query>
+        </div>
+      </ApolloProvider>
     );
   }
 }
